d38d2502ef5c302d882032d05823ecbd0c42736b
nguyenhohoangvy <nguyenhohoangvy@gmail.com>
2026-04-13 14:25:50 +0700

    .

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://server-dev.nguyenhohoangvy.workers.dev/',
+  API_URL: 'https://uth-vpswallet-server-dev.nguyenhohoangvy.workers.dev/',
   API_LOCAL_URL: 'http://127.0.0.1:8787/',
 };
